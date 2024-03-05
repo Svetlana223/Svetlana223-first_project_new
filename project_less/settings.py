d4f6e9523d90ce14c1844294789a6e2e0c890296
Svetlana <strel_sv@bk.ru>
2024-03-04 21:19:29 -0500

    add changes2

--- project_less/settings.py
+++ project_less/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get("MYSQL_DBNAME"),
         'USER': os.environ.get("MYSQL_USER"),
-        'PASSWORD': 'os.environ.get("MYSQL_PASSWORD")',
+        'PASSWORD': 'Svetlana20',
         'HOST': os.environ.get("MYSQL_HOST"),
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
